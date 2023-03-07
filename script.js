7083e0c286a0301a6b560e5e5a085ea4b9917bf7
Don Artkins <opiyodon9@gmail.com>
2023-03-07 18:38:13 +0300

    code update finished my class project assignment

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 /* ====================================================================================================================
+  ================================   LINK ACTIVE NAV   ================================================================
+  ==================================================================================================================== */
+  var linkNav = document.querySelectorAll('.NAV_LI')
+
+  function active(){
+      if(linkNav){
+          linkNav.forEach(l=> l.classList.remove('active'))
+          this.classList.add('active')
+      }
+  }
+  linkNav.forEach(l=> l.addEventListener('click',active))
+  //sectionScroll
+    /*var sectionScroll = document.querySelectorAll('.sectionScroll');
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY = sectionScroll) {
+            linkNav.forEach(l=> l.classList.remove('active'))
+            this.classList.add('active')
+        }
+    })
+/* ====================================================================================================================
 ================================   GO TO TOP BUTTON ||| RESUSABLE CODE   ================================================================
 ==================================================================================================================== */
 //function to go to the top the page when button is clicked || its appears only after scrolling for some distance from the top of the screen
